50d06b5ec9b737cf282eaa9f9153dc0ddb302f26
cherlau <cherlau.prado@appfacilita.com>
2026-05-02 12:11:03 -0300

    Add Jest and Supertest for testing

--- frontend/src/views/ContactFormView.vue
+++ frontend/src/views/ContactFormView.vue
@@ -9,19 +9,19 @@ const { form, errors, loading, isEditing, handleFileChange, handleSubmit } = use
     <h1>{{ isEditing ? 'Editar contato' : 'Novo contato' }}</h1>
     <form @submit.prevent="handleSubmit">
       <div>
-        <input v-model="form.name" type="text" />
+        <input v-model="form.name" type="text" placeholder="Nome" />
         <p v-if="errors.name">{{ errors.name }}</p>
       </div>
       <div>
-        <input v-model="form.contact" type="text" />
+        <input v-model="form.contact" type="text" placeholder="Telefone" />
         <p v-if="errors.contact">{{ errors.contact }}</p>
       </div>
       <div>
-        <input v-model="form.email" type="email" />
+        <input v-model="form.email" type="email" placeholder="E-mail" />
         <p v-if="errors.email">{{ errors.email }}</p>
       </div>
       <div>
-        <input type="file" @change="handleFileChange" />
+        <input type="file" @change="handleFileChange" accept="image/*" />
         <p v-if="errors.picture">{{ errors.picture }}</p>
       </div>
       <p v-if="errors.general">{{ errors.general }}</p>

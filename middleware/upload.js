6a65e022c4ccf5d920231d2092ab156ba4f2c476
cherlau <cherlau.prado@appfacilita.com>
2026-05-02 08:56:30 -0300

    add multer middleware for image uploads

---
+++ middleware/upload.js
@@ -0,0 +1,19 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, '..', 'uploads'),
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Apenas arquivos de imagem são permitidos'), false);
+  }
+};
+
+module.exports = multer({ storage, fileFilter }).single('picture');

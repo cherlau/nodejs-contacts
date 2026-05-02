83a285067d946c8ed66e35a1e4ba2a06d759c944
cherlau <cherlau.prado@appfacilita.com>
2026-05-02 10:07:23 -0300

    update database configuration with connection pool settings

--- config/database.js
+++ config/database.js
@@ -6,6 +6,10 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 module.exports = pool;
